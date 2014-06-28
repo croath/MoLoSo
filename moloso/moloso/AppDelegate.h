4fd57988f9e4287c038db32228f04984dc274bed
croath <croathliu@gmail.com>
2014-06-28 15:15:11 +0800

    base

---
+++ moloso/moloso/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  moloso
+//
+//  Created by croath on 6/28/14.
+//  Copyright (c) 2014 croath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
