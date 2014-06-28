7cd882eb8490d21f2f828e833c84e704c5ce6ef3
croath <croathliu@gmail.com>
2014-06-28 20:51:59 +0800

    view controller

---
+++ moloso/moloso/controllers/qrcode/QRCodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  QRCodeViewController.h
+//  moloso
+//
+//  Created by croath on 6/28/14.
+//  Copyright (c) 2014 croath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QRCodeViewController : UIViewController
+
+@end
---
+++ moloso/moloso/controllers/qrcode/ScanViewController.m
@@ -0,0 +1,38 @@
+//
+//  ScanViewController.m
+//  moloso
+//
+//  Created by croath on 6/28/14.
+//  Copyright (c) 2014 croath. All rights reserved.
+//
+
+#import "ScanViewController.h"
+
+@interface ScanViewController ()
+
+@end
+
+@implementation ScanViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
